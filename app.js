b304f6d325476e10beed2d12b41d06d12b5938b5
asouzay <161075027+asouzay@users.noreply.github.com>
2025-11-25 14:19:15 -0400

    ativ3

--- app.js
+++ app.js
@@ -20,6 +20,22 @@ app.get("/tico", (req, res) => {
   res.send("teco");
 });
 
+app.get("/pokemons", (req, res) => {
+  const pokemons = [
+    "Caterpie",
+    "Pidgeotto",
+    "Bulbasaur",
+    "Charmander",
+    "Squirtle",
+    "Krabby",
+    "Primeape",
+    "Muk",
+    "Tauros",
+    "Snorlax"
+  ];
+
+  res.json(pokemons);
+});
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
